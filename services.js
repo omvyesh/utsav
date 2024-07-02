8eee037d2199f21ab71b8e0b2283ba2837846e23
Divyesh Shivdas Swarge <154859858+omvyesh@users.noreply.github.com>
2024-07-02 18:17:23 +0530

    Update services.js

--- services.js
+++ services.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cards = document.querySelectorAll('.card');
-    
-    cards.forEach(card => {
-        card.addEventListener('mouseover', () => {
-            card.classList.add('hover');
-        });
-        
-        card.addEventListener('mouseout', () => {
-            card.classList.remove('hover');
-        });
-    });
-});
+
